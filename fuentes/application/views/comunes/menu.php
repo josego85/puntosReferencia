ba382dd4f605babe08b5fe1d5dac4f3a81207c18
proyectosbeta <josego85@gmail.com>
2018-09-16 20:41:14 -0400

    Mejora en el menu.

--- fuentes/application/views/comunes/menu.php
+++ fuentes/application/views/comunes/menu.php
@@ -1,6 +1,13 @@
 <!-- Inicio del menu -->
-<ul class="nav nav-pills nav-justified">
-    <li class="active"><a href="<?php echo base_url();?>">Inicio</a></li>
-    <li><a href="<?php echo base_url() . 'inicio/marcar_punto_referencia';?>"">Marcar punto</a></li>
-</ul>
+<nav class="navbar navbar-expand-sm bg-light">
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo base_url();?>">Inicio</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="<?php echo base_url() . 'inicio/marcar_punto_referencia';?>">Marcar punto</a>
+    </li>
+  </ul>
+</nav>
 <!-- Fin del menu -->
